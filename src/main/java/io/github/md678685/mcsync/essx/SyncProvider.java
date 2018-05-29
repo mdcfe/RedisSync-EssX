fe0eeb95d1103da63ca6b201c0576c9c5d5c643a
md678685 <mathew678@hotmail.co.uk>
2018-05-29 18:29:16 +0100

    Add mail clear and mute propagation

--- src/main/java/io/github/md678685/mcsync/essx/SyncProvider.java
+++ src/main/java/io/github/md678685/mcsync/essx/SyncProvider.java
@@ -1,7 +1,6 @@
 package io.github.md678685.mcsync.essx;
 
-import com.earth2me.essentials.User;
-
+import com.earth2me.essentials.UserData;
 import net.ess3.api.sync.AbstractSyncProvider;
 import io.github.md678685.redisqueue.RedisQueue;
 
@@ -17,19 +16,32 @@ public class SyncProvider extends AbstractSyncProvider {
     }
 
     @Override
-    public void addMail(User userData, String msg) {
-        SyncPlugin.getInstance().getLogger().info("Sending outbound mail from " + userData.getLastAccountName());
-        queue.publishMessage("MAIL to " + userData.getConfigUUID() + " reads " + msg);
+    public void addMail(UserData user, String msg) {
+        SyncPlugin.getInstance().getLogger().info("Sending mail add for " + user.getLastAccountName());
+        queue.publishMessage("MAIL to " + user.getConfigUUID() + " reads " + msg);
     }
 
     @Override
-    public void setNickname(User userData, String nick) {
-        SyncPlugin.getInstance().getLogger().info("Sending nickname change for " + userData.getLastAccountName());
-        queue.publishMessage("NICK of " + userData.getConfigUUID() + " now " + nick);
+    public void clearMail(UserData user) {
+        SyncPlugin.getInstance().getLogger().info("Sending mail clear for " + user.getLastAccountName());
+        queue.publishMessage("MAIL of " + user.getConfigUUID() + " clear");
     }
 
-	@Override
-	public void clearMail(User arg0, String arg1) {
-		
-	}
+    @Override
+    public void setNickname(UserData user, String nick) {
+        SyncPlugin.getInstance().getLogger().info("Sending nickname change for " + user.getLastAccountName());
+        queue.publishMessage("NICK of " + user.getConfigUUID() + " now " + nick);
+    }
+
+    @Override
+    public void setMuted(UserData user, boolean state) {
+        SyncPlugin.getInstance().getLogger().info("Sending mute change for " + user.getLastAccountName());
+        queue.publishMessage("MUTE of " + user.getConfigUUID() + " is " + state);
+    }
+
+    @Override
+    public void setMuteTimeout(UserData user, long time) {
+        SyncPlugin.getInstance().getLogger().info("Sending mute change for " + user.getLastAccountName());
+        queue.publishMessage("MUTE of " + user.getConfigUUID() + " timeout " + time);
+    }
 }
